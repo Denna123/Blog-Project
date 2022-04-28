e99bfb163699726ca98ca8718e5d6d07dee2f313
Denna123 <denna5454@gmail.com>
2022-04-28 20:50:32 +0800

    create compose

--- app.js
+++ app.js
@@ -32,6 +32,15 @@ app.get("/contact", function(req, res) {
   res.render("contact", {contactPage: contactContent});
 });
 
+app.get("/post", function(req, res) {
+  res.render("post");
+});
+
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
