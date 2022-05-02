b1adff060f64ffe4f8cf2fc09662c8547d98faef
Denna123 <denna5454@gmail.com>
2022-05-03 00:22:39 +0800

    pages create

--- app.js
+++ app.js
@@ -3,8 +3,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash'); 
 
-const posts = ["Post1", "Post2", "Post3"];
+let posts = [];
+
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -30,7 +32,10 @@ app.post("/", function(req,res) {
 
 
 app.get("/" , function(req, res) {
-  res.render("home", {startingContent: homeStartingContent});
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+  });
 });
 
 
@@ -44,7 +49,10 @@ app.get("/contact", function(req, res) {
 });
 
 app.get("/post", function(req, res) {
-  res.render("post");
+  res.render("home", {
+    storedTitle: storedTitle,
+    posts: posts
+  });
 });
 
 app.get("/compose", function(req, res) {
@@ -52,13 +60,35 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  const post = (req.body.postTitle);
-  postTitle.push(post);
+  // console.log(req.body.postTitle);
+
+  const post = {
+    title:  req.body.postTitle,
+    content:  req.body.postBody
+  };  
+  posts.push(post);
   res.redirect("/");
 });
 
+
+app.get("/posts/:postName", function(req, res) {
+  let requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+
+  if(storedTitle === requestedTitle) {
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    })
+  } 
+
+  });
+});
 
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+  
